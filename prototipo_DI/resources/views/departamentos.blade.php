f316687155383aca631c9ec09db83fb2ca3e77cc
Alexrex12 <alexrex192002@gmail.com>
2023-03-09 15:06:25 -0600

    primer entrega de del diseño de interfaz

--- prototipo_DI/resources/views/departamentos.blade.php
+++ prototipo_DI/resources/views/departamentos.blade.php
@@ -56,7 +56,7 @@
 		</div>
 	</div>
     <center>
-        <button data-tooltip-target="tooltip-assign" data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Asignar ticket</button>
+        <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Asignar ticket</button>
         <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Regresar</button>
     </center>
 <!--Esta sera la tabla en donde se mostrará la informacion-->
