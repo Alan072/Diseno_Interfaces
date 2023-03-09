dc447c224896ba359bb02e098e59ae0192c9a748
Alan072 <91845623+Alan072@users.noreply.github.com>
2023-03-09 14:49:15 -0600

    Vista departamento

--- prototipo_DI/resources/views/jefe_ticket_editar.blade.php
+++ prototipo_DI/resources/views/jefe_ticket_editar.blade.php
@@ -1,5 +1,7 @@
 @extends ('plantilla_jefe')
 @section ('contenido')
+@include('modalEjem')
+
 
 <form>
     <div class="grid gap-6 mb-6 md:grid-cols-2">
@@ -40,7 +42,7 @@
     </label>
 </div> 
 <center>
-    <button type="button" class="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Actualizar</button>
+    <button data-modal-target="actualizar" data-modal-toggle="actualizar" type="button" class="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Actualizar</button>
 
 </center>
 
