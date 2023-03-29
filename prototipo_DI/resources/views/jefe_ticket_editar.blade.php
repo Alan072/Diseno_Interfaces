1132edb8793cd0c17fb4a9729390ee54d9f9dc83
Alan072 <91845623+Alan072@users.noreply.github.com>
2023-03-29 11:26:48 -0600

    password anadido

--- prototipo_DI/resources/views/jefe_ticket_editar.blade.php
+++ prototipo_DI/resources/views/jefe_ticket_editar.blade.php
@@ -88,15 +88,34 @@
               ?>
             </select>
           </div>
-
-
             <div class="mb-6">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
                 <input type="email" id="email" name="email" value="{{ $empleado->email}}"class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"  required>
             </div> 
-          </div>
 
-       
+            <div class="mb-6">
+              <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+              <div class="relative">
+                <input type="password" id="password" name="password" value="{{ $empleado->password}}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="********" required>
+                <button type="button" class="absolute inset-y-0 right-0 px-3 py-2 bg-gray-200 rounded-md text-gray-700 dark:bg-gray-500 dark:text-gray-200" onclick="togglePasswordVisibility()">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                      <path fill-rule="evenodd" d="M10 12a2 2 0 100-4 2 2 0 000 4zM2.9 9c.2-.7.5-1.4 1.1-1.9A13 13 0 0110 5c2.2 0 4.2.5 5.9 1.4.6.4 1 1 1.2 1.6l-1.8.9c-.1-.4-.4-.7-.7-.9A8.4 8.4 0 0010 7c-1.3 0-2.5.3-3.5.9-.2.1-.4.2-.6.2-.2 0-.4-.1-.6-.2l-1.8-.9zM5 9a3 3 0 116 0 3 3 0 01-6 0z" clip-rule="evenodd" />
+                      <path fill-rule="evenodd" d="M10 14.5c-2.2 0-4.2-.5-5.9-1.4-.6-.4-1-1-1.2-1.6l1.8-.9c.1.4.4.7.7.9a8.4 8.4 0 003.9 1.6c1.3 0 2.5-.3 3.5-.9.2-.1.4-.2.6-.2.2 0 .4.1.6.2l1.8.9c-.2.5-.5 1.1-1.1 1.6A13 13 0 0110 15.5z" clip-rule="evenodd" />
+                  </svg>
+              </button>
+              </div>
+            </div>
+          </div>
+          <script>
+            function togglePasswordVisibility() {
+                var passwordInput = document.getElementById("password");
+                if (passwordInput.type === "password") {
+                    passwordInput.type = "text";
+                } else {
+                    passwordInput.type = "password";
+                }
+            }
+        </script>
     <br>
     <button type="submit" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Actualizar</button>
     </form>
@@ -114,5 +133,18 @@
     }
   </style>
 
-
+<script>
+  function togglePasswordVisibility() {
+    var passwordField = document.getElementById("password");
+    var visibilityButton = document.querySelector("#password + button");
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      visibilityButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+<path fill-rule="evenodd" d="M10 12a2 2 0 100-4 2 2 0 000 4zM2.9 9c.2-.7.5-1.4 1.1-1.9A13 13 0 0110 5c2.2 0 4.2.5 5.9 1.4.6.4 1 1 1.2 1.6l-1.8.9c-.1-.4-.4-.7-.7-.9A8.4 8.4 0 0010 7c-1.3 0-2.5.3-3.5.9-.2.1-.4.2-.6.2-.2 0-.4-.1-.6-.2l-1.8-.9zM5 9a3 3 0 116 0 3 3 0 01-6 0z" clip-rule="evenodd"><path fill-rule="evenodd" d="M10 14.5c-2.2 0-4.2-.5-5.9-1.4-.6-.4-1-1-1.2-1.6l1.8-.9c.1.4.4.7.7.9a8.4 8.4 0 003.9 1.6c1.3 0 2.5-.3 3.5-.9.2-.1.4-.2.6-.2.2 0 .4.1.6.2l1.8.9c-.2.5-.5 1.1-1.1 1.6A13 13 0 0110 15.5z" clip-rule="evenodd"></svg>';
+} else {
+passwordField.type = "password";
+visibilityButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5"><path fill-rule="evenodd" d="M10 14.5c-2.2 0-4.2-.5-5.9-1.4-.6-.4-1-1-1.2-1.6l1.8-.9c.1.4.4.7.7.9a8.4 8.4 0 003.9 1.6c1.3 0 2.5-.3 3.5-.9.2-.1.4-.2.6-.2.2 0 .4.1.6.2l1.8.9c-.2.5-.5 1.1-1.1 1.6A13 13 0 0110 15.5zM10 12a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></svg>';
+}
+}
+</script>
 @stop
