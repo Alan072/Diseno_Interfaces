d294386d40321ece4a5c8bce2b75121450221f45
Alan072 <91845623+Alan072@users.noreply.github.com>
2023-03-30 16:16:13 -0600

    avance

--- prototipo_DI/resources/views/clientticket.blade.php
+++ prototipo_DI/resources/views/clientticket.blade.php
@@ -9,17 +9,70 @@
                 <input type="number" id="nombre" name="nombre" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="12345" required>
             </div>
             <div>
-                <label for="nombre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Autor</label>
-                <input type="text" id="nombre" name="nombre" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Alejandro" required>
+                <label for="autor" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Autor</label>
+                <select id="autor" class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <option disabled selected>Autor del Ticket</option>
+                    <?php
+                // Realizar la conexión a la base de datos
+                $conexion = mysqli_connect("localhost:3307", "root", "", "prototipo_di");
+          
+                // Verificar la conexión
+                if (!$conexion) {
+                  die("Error al conectar a la base de datos: " . mysqli_connect_error());
+                }
+          
+                // Realizar la consulta a la base de datos
+                $query = "SELECT * FROM empleado where puesto_id= 3 ";
+                $resultado = mysqli_query($conexion, $query);
+          
+                // Verificar si la consulta devolvió resultados
+                if (mysqli_num_rows($resultado) > 0) {
+                  // Iterar sobre los resultados y crear los elementos de la lista desplegable
+                  while ($fila = mysqli_fetch_assoc($resultado)) {
+                    echo "<option value='" . $fila["id_empleado"] . "'>" . $fila["nombre"] . " " . $fila["apellido_paterno"] . " " . $fila["apellido_materno"] . "</option>";
+                  }
+                } else {
+                  // Si la consulta no devolvió resultados, mostrar un mensaje de error
+                  echo "<option value=''>No se encontraron resultados</option>";
+                }
+          
+                // Cerrar la conexión a la base de datos
+                mysqli_close($conexion);
+              ?>
+            </select>
             </div>
             <div>
-                <label for="small" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Departamento</label>
-                <select id="small" class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <label for="departamento" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Departamento</label>
+                <select id="departamento" class="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option disabled selected>Selecciona un departamento</option>
-                    <option  value="US">Lógistica</option>
-                    <option value="CA">Contabilidad</option>
-                    <option value="FR">Negocios</option>
-                </select>
+                    <?php
+                // Realizar la conexión a la base de datos
+                $conexion = mysqli_connect("localhost:3307", "root", "", "prototipo_di");
+          
+                // Verificar la conexión
+                if (!$conexion) {
+                  die("Error al conectar a la base de datos: " . mysqli_connect_error());
+                }
+          
+                // Realizar la consulta a la base de datos
+                $query = "SELECT * FROM departamento";
+                $resultado = mysqli_query($conexion, $query);
+          
+                // Verificar si la consulta devolvió resultados
+                if (mysqli_num_rows($resultado) > 0) {
+                  // Iterar sobre los resultados y crear los elementos de la lista desplegable
+                  while ($fila = mysqli_fetch_assoc($resultado)) {
+                    echo "<option value='" . $fila["id_departamento"] . "'>" . $fila["nombre"] . "</option>";
+                  }
+                } else {
+                  // Si la consulta no devolvió resultados, mostrar un mensaje de error
+                  echo "<option value=''>No se encontraron resultados</option>";
+                }
+          
+                // Cerrar la conexión a la base de datos
+                mysqli_close($conexion);
+              ?>
+            </select>
             </div>
             <div>
                 <label for="nombre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha</label>
--- prototipo_DI/resources/views/jefe_ticket.blade.php
+++ prototipo_DI/resources/views/jefe_ticket.blade.php
@@ -20,9 +20,6 @@
                       <input name="imagen" id="imagen" required type='file' class="hidden" />
                   </label>
               </div>
-          </div>
-      </div>
-      
           <div>
             <label for="nombre" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
             <input type="text" id="nombre" name="nombre" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Alan" required>
